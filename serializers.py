c31b10cc1c708e0d10528b0658bb33f20076cf99
Asim Farooq <asimfarooq5@gmail.com>
2020-02-07 12:23:29 +0500

    fixes

--- serializers.py
+++ serializers.py
@@ -33,7 +33,7 @@ class SubmissionSchema(ma.Schema):
 class ScoreSchema(ma.Schema):
     class Meta:
         model = Score
-        fields = ('score', 'angler', 'specie', 'competition')
+        fields = ('comp_uid', 'score', 'angler', 'specie', 'competition')
 
 
 class ImageSchema(ma.Schema):
